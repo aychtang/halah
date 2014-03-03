16ddeaed0853a8b4f6774fcf53bac26823e2fdde
Howard Tang <tangh@google.com>
2014-03-03 15:18:28 +0000

    Use es5 array methods where possible. Fix context reference bug.

--- recompute.js
+++ recompute.js
@@ -9,16 +9,16 @@ var requireFlush = function() {
 };
 
 var flush = function() {
-	for (var i = 0; i < toFlush.length; i++) {
-		toFlush[i].compute();
-	}
+	toFlush.forEach(function(fn) {
+		fn.compute();
+	});
 	toFlush = [];
 };
 
 var contains = function(array, obj) {
-	for (var i = 0; i < array.length; i++) {
-		if (array[i] === obj) return true;
-	}
+	array.some(function(e) {
+		return e === obj;
+	});
 };
 
 // Keeps reference to a function.
@@ -59,7 +59,7 @@ Dependency.prototype.depend = function() {
 		computationId = computation.id;
 		this.dependents[computationId] = computation;
 		computation.onInvalidateCallbacks.push(function() {
-			delete self.dependents[computationId];
+			delete this.dependents[computationId];
 		}.bind(this));
 	}
 };
